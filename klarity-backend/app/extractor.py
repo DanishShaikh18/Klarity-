7e696fdc52ae0b52e67d2adbf4d52c678a2d96c5
Danish_Shaikh <danish89761@gmail.com>
2025-10-22 00:11:44 +0530

    Learned more about Extraction

--- klarity-backend/app/extractor.py
+++ klarity-backend/app/extractor.py
@@ -1,14 +1,18 @@
-import pdfplumber
 import fitz
 
+data = fitz.open(r'data/5thPractical.pdf')
 
+print("Total Pages ", data.page_count)
 
-file_path = r'data/BasicConceptualQuestions-Python.pdf'
+for i , page in enumerate(data):
+    image_list = page.get_images(full=True)
+    print(f"Page {i+1} has {len(image_list)} images")
 
+    for img_index , img in enumerate(image_list):
+        xref = img[0]
+        base_image = data.extract_image(xref)
+        image_bytes = base_image["image"]
+        image_ext = base_image["ext"]
 
-with pdfplumber.open(file_path) as pdf:
-    for page in pdf.pages:
-        print(page.extract_text())
-    
-
-    
+        with open(f"image {i+1}_{img_index+1}.{image_ext}", "wb") as img_file:
+            img_file.write(image_bytes)
